4ac6a6cd0d94a6d2667348e872d884f2121b861f
Dark-Knight11 <vedantbhamare11@gmail.com>
2021-10-22 16:56:07 +0530

    Initial Commit

---
+++ calculator.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    console.log(num1, num2, result);
+    res.send("The result is " + result);
+});
+
+app.get("/bmicalculator", (req, res) => {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", (req, res) => {
+    var wt = Number(req.body.wt);
+    var ht = Number(req.body.ht);
+    var result = (wt / ht ** 2).toFixed(2);
+    res.send("Your BMI is " + result + "%");
+});
+
+
+app.listen(port, () => {
+    console.log("Server Started on port 3000");
+});
